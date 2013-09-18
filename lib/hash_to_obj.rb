3d19be82b1ee9146197495fe296099b29ddd176a
Michael Chittenden <mchitten@gmail.com>
2013-09-18 14:18:39 -0400

    Actual logic

--- lib/hash_to_obj.rb
+++ lib/hash_to_obj.rb
@@ -1,5 +1,16 @@
 require "hash_to_obj/version"
 
-module HashToObj
-  # Your code goes here...
+class Hash
+  def to_obj
+    hash = self
+    klass = Class.new
+    hash.each do |key, val|
+      klass.instance_variable_set("@#{key}", val)
+      klass.send(:define_method, key, proc { klass.instance_variable_get("@#{key}") })
+      klass.send(:define_method, "#{key}=", proc { |val| klass.instance_variable_set("@#{key}", val) })
+    end
+
+    klass.new
+  end
 end
+
